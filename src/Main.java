0db96987f0e4b43061b4d8412c43e32d2caaa594
goldenromeo <rommeltrejo@gmail.com>
2016-02-02 15:49:46 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+class Main{
+
+
+    public static void main(String[] args) {
+
+        print("hello world");
+
+
+    }
+
+
+
+    private static void print(String output){
+
+        System.out.println(output);
+
+    }
+
+}
